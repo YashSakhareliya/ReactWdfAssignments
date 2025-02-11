afdf2c692ac1ecba1412306dc9785d6cdb66920c
Yash sakhareliya <sakhareliyayash@gmail.com>
2025-02-11 17:02:17 +0530

    added Task 3

---
+++ Task3/src/App.jsx
@@ -0,0 +1,19 @@
+import Footer from "./components/Footer"
+import Header from "./components/Header"
+import MainComponent from "./components/MainComponent"
+
+
+
+function App() {
+  
+
+  return (
+    <>
+     <Header />
+     <MainComponent />
+     <Footer />
+     </>
+  )
+}
+
+export default App
